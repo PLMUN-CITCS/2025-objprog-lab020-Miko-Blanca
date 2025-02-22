b7124539dbe2e594daf9f2a68a24a087050ddf72
Miko-Blanca <mikoblnca@gmail.com>
2025-02-23 01:00:25 +0800

    Submitting OBJPROG Week 05 - Laboratory # 20

---
+++ MenuDrivenProgram.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+public class MenuDrivenProgram {
+
+    public static void displayMenu() {
+        System.out.println("Menu:");
+        System.out.println("1. Greet User");
+        System.out.println("2. Check Even/Odd");
+        System.out.println("3. Exit");
+        System.out.print("Enter your choice (1-3): ");
+    }
+    public static boolean handleMenuChoice(int choice) {
+        Scanner scanner = new Scanner(System.in);
+
+        switch (choice) {
+            case 1:
+                greetUser(); 
+                break;
+            case 2:
+                checkEvenOrOdd(); 
+                break;
+            case 3:
+                System.out.println("Exiting program. Goodbye!"); 
+                return true;
+            default:
+                System.out.println("Invalid choice. Please enter 1, 2, or 3."); 
+                break;
+        }
+        return false; 
+    }
+    public static void greetUser() {
+        System.out.println("Hello! Welcome!");
+    }
+    public static void checkEvenOrOdd() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter an integer: ");
+        int number = scanner.nextInt();
+    }
+}
